619baa6f53390274bc931dad2568bb7e3ba5e3b3
Jeremy Childers <jlchilders11@k2m.com>
2020-02-26 01:31:25 -0500

    initial

---
+++ core/views/views.py
@@ -0,0 +1,8 @@
+'''This is where the views for this application live. All of them are class based, so that they are futureproofed and hotswappable'''
+from django.views.generic import TemplateView
+
+class HomeView(TemplateView):
+	template_name = 'home.html'
+
+class ProjectsView(TemplateView):
+	template_name = 'projects.html'
